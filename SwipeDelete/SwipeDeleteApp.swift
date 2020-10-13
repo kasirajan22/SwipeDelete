4a352e213c5669e82b3efae12004cc21a564898d
kasirajan22 <kasi.srm@gmail.com>
2020-10-13 06:37:59 +0530

    Initial Commit

---
+++ SwipeDelete/SwipeDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeDeleteApp.swift
+//  SwipeDelete
+//
+//  Created by Magizhan on 13/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwipeDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
